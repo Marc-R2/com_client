d4aa5187a978c81c47c9daf7683044253915f93c
Marc Renken <marc.renken@bitbot.eu>
2022-10-09 14:32:55 +0200

    feature(Deeper direct request in LocalField(s)):
    Signed-off-by: Marc Renken <marc.renken@bitbot.eu>

--- lib/local/local_field.dart
+++ lib/local/local_field.dart
@@ -1,7 +1,30 @@
 part of '../com_client.dart';
 
 class LocalField<T extends LocalTask> {
-  const LocalField({required this.name});
+  const LocalField({required this.name, required this.task});
 
   final String name;
+
+  final T task;
+
+  TaskRequest? request({
+    String? origin,
+    String serviceVersion = '^',
+    String connectType = 'service',
+    void Function(TaskAnswer answer)? onComplete,
+    Map<String, dynamic> data = const <String, dynamic>{},
+    String destination = 'none',
+    String? taskID,
+  }) {
+    return task.request(
+      fields: [this],
+      origin: origin,
+      serviceVersion: serviceVersion,
+      connectType: connectType,
+      onComplete: onComplete,
+      data: data,
+      destination: destination,
+      taskID: taskID,
+    );
+  }
 }
